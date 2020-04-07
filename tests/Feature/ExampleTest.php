f35d2eb85481239d5f2875c175347de872dd4cba
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-04-07 23:53:02 +0200

    Initial commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Blog\Tests\Feature;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
